74841a6eb4a6f262f07c68372d63c75de60f8615
xipeRafa <rafaelsistem87@gmail.com>
2018-10-29 18:58:33 -0700

    device problems

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
-class Article < ApplicationRecord
+class Article < ActiveRecord::Base
+	belongs_to :user
+	belongs_to :category
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_30_003141) do
+ActiveRecord::Schema.define(version: 2018_10_30_011652) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "category_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
